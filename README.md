44aa496010bdf44b224bd6df5549000c488b9218
Marc Serdeliuc <serdeliuk@yahoo.com>
2020-08-11 10:52:10 +0200

    Release 1.0.8

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VirtualBiosMod
 
 #### Bios MOD without hardware flashing requirement
-[![download](https://img.shields.io/github/downloads/serdeliuk/VirtualBiosMod/total)](https://github.com/serdeliuk/VirtualBiosMod/releases/download/7/VirtualBiosMod.1.0.7.zip)
+[![download](https://img.shields.io/github/downloads/serdeliuk/VirtualBiosMod/total)](https://github.com/serdeliuk/VirtualBiosMod/releases/download/8/VirtualBiosMod.1.0.8.zip)
 
 ## This source code is licensed under [GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0.html#preamble)
 ## Please read LICENSE.txt for more details
@@ -11,8 +11,8 @@ This virtual mod is not permament and if your new settings goes bad you need to 
 
 It was tested only on Asus ROG STRIX G531GT 
 
-This should work with any UEFI bios regardless the operating system,if you have Clover booloader put the VirtualBioosMod.efi in drivers and reboot.<br>
-If your booloader does not know how to load efi drivers put the VirtualBiosMod.efi on a fat32 usb stick and create in your bios a new boot entry and point to VirtualBiosMod.efi file.<br>
+This should work with any UEFI bios regardless the operating system, if you have Clover booloader put the VirtualBioosMod.efi in drivers and reboot.<br>
+If your bootloader does not know how to load efi drivers put the VirtualBiosMod.efi on a fat32 usb stick and create in your bios a new boot entry and point to VirtualBiosMod.efi file.<br>
 Put the new entry as first boot and the second boot put OS which want to boot after exit from VirtualBiosMod.efi app
 Please refer to [Wiki](https://github.com/serdeliuk/VirtualBiosMod/wiki) for more details
 
--- VirtualBiosMod.c
+++ VirtualBiosMod.c
@@ -15,9 +15,7 @@
         GNU General Public License for more details.
 
         You should have received a copy of the GNU General Public License
-        along with VirtualBiosMod.  If not, see <http://www.gnu.org/licenses/>
-
-        Please keep this header in all your source codes.
+        along with VirtualBiosMod.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
@@ -25,13 +23,12 @@
 #include <efilib.h>
 #include "functions.c"
 
-
 EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 {
     int changes = 0;
     int vmajor = 1;
     int vminor = 0;
-    int vpatch = 6;
+    int vpatch = 8;
     int params = 0;
 
     EFI_STATUS status;
@@ -54,54 +51,49 @@ EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
             Print(L"[VirtualBiosMod] err: %r\n", status);
     } else {
 	params = loaded_image->LoadOptionsSize;
-//            Print(L"[VirtualBiosMod] params: %d\n", params);
     }
 
     uefi_call_wrapper(ST->ConOut->ClearScreen, 1, ST->ConOut);
     if ( params == 0 ){
-	Print(L"\n\n\n[VirtualBiosMod v%d.%d.%d] Press ",vmajor,vminor,vpatch);
-	uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLUE);
-	Print(L"del");
-	uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLACK);
-	Print(L" or any other key to enter Setup...");
+    Print(L"\n\n\n[VirtualBiosMod v%d.%d.%d] Press ",vmajor,vminor,vpatch);
+    uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLUE);
+    Print(L"del");
+    uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLACK);
+    Print(L" or any other key to enter Setup...");
 
-	WaitForSingleEvent(ST->ConIn->WaitForKey, 10000000); // 10000000 = one second
+    WaitForSingleEvent(ST->ConIn->WaitForKey, 10000000); // 10000000 = one second
 
-	while (!exit) {
+    while (!exit) {
 
-	efi_status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &efi_input_key);
+    efi_status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &efi_input_key);
 
-	    if (efi_status != EFI_SUCCESS) {
-		Print(L" Exiting\n\n\n");
-		return EFI_SUCCESS;
-	    } else {
-		break;
-	    }
+	if (efi_status != EFI_SUCCESS) {
+	    Print(L" Exiting\n\n\n");
+	    return EFI_SUCCESS;
+	} else {
+	    break;
 	}
+    }
     }
 
     uefi_call_wrapper(ST->ConOut->ClearScreen, 1, ST->ConOut);
     uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLUE);
 
-
+//  Print(L"123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_");
 /////////--DRAW MAIN BLUE BOX--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     Print(L"             SERDELIUK - VirtualBiosMod v%d.%d.%d CMOS Setup Utility               ",vmajor,vminor,vpatch);
-    draw_box_simple(80, 11, 0, 1);
-
+    draw_box_simple(80, 8, 0, 1);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 2); // h, v ;pos
+    Print(L"Firmware version:       %d.%02d", ST->FirmwareRevision >> 16, ST->FirmwareRevision & 0xffff);
+    Print(L" (%s)", ST->FirmwareVendor,L"%c"); // firmware vendoor
 
     uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 3); // h, v ;pos
     Print(L"UEFI version:           %d.%02d", ST->Hdr.Revision >> 16, ST->Hdr.Revision & 0xffff);
 
-    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 4); // h, v ;pos
-    Print(L"Firmware vendor:        %s", ST->FirmwareVendor,L"%c");
-
-    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 5); // h, v ;pos
-    Print(L"Firmware version:       %d.%02d", ST->FirmwareRevision >> 16, ST->FirmwareRevision & 0xffff);
-
-    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 0, 15);
+    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 0, 12);
     status = get_bios_variables( &guid, L"Setup", &data, &data_size, attr);
     if (status != EFI_SUCCESS) {
 	uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_RED|EFI_BACKGROUND_BLACK);
@@ -117,48 +109,119 @@ EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
     int offset_lock = 0xb4b;
     int offset_video = 0xa45;
     int offset_audio = 0x108E;
+    int offset_xtu = 0x859;
+    int offset_overclock = 0x858;
+    int offset_ratio = 0x85A;
+    int offset_ring = 0x865;
+    int offset_maxring = 0x8AC;
+    int offset_adaptive = 0x40F;
+    int offset_avx = 0x863;
 
 redraw:
     WaitForSingleEvent(ST->ConIn->WaitForKey, 10); // 10000000 = one second
     uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLUE);
+
+//      Print(L"123456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789_");
     if ( data[offset_lock] == 0) {
-	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 7); // h, v ;pos
-	Print(L"B.I.O.S. status:        Unlocked");
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 5); // h, v ;pos
+	Print(L"B.I.O.S. status:        Unlocked      ");
 	} else {
-	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 7); // h, v ;pos
-	Print(L"B.I.O.S. status:        Locked  ");
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 5); // h, v ;pos
+	Print(L"B.I.O.S. status:        Locked        ");
     }
 
     if ( data[offset_video] == 0) {
-	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 8); // h, v ;pos
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 6); // h, v ;pos
 	Print(L"Video card:             IGFX -- Intel ");
 	} else {
-	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 8); // h, v ;pos
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 6); // h, v ;pos
 	Print(L"Video card:             SG   -- Nvidia");
     }
 
     if ( data[offset_audio] == 0) {
-	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 9); // h, v ;pos
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 7); // h, v ;pos
 	Print(L"HDMI Audio:             Enabled       ");
 	} else {
-	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 9); // h, v ;pos
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 7); // h, v ;pos
 	Print(L"HDMI Audio:             Disabled      ");
     }
 
-    uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLACK);
-    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 0, 15);
-    Print(L"\n\n Press V to switch video card\n");
-    Print(L" Press B to unlock the bios\n");
+    if ( data[offset_xtu] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 8); // h, v ;pos
+	Print(L"Intel  XTU:             Disabled   ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 8); // h, v ;pos
+	Print(L"Intel  XTU:             Enabled    ");
+    }
 
-    Print(L" Press D to draw a box\n");
+    if ( data[offset_adaptive] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 9); // h, v ;pos
+	Print(L"Adaptive performance:   Disabled   ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 3, 9); // h, v ;pos
+	Print(L"Adaptive performance:   Enabled    ");
+    }
+
+    if ( data[offset_overclock] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 5); // h, v ;pos
+	Print(L"Overclock :             Disabled   ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 5); // h, v ;pos
+	Print(L"Overclock :             Enabled    ");
+    }
+
+    if ( data[offset_ratio] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 6); // h, v ;pos
+	Print(L"Core RATIO:             Default    ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 6); // h, v ;pos
+	Print(L"Core RATIO:             53         ");
+    }
+
+    if ( data[offset_ring] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 7); // h, v ;pos
+	Print(L"Core Ring:              Default    ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 7); // h, v ;pos
+	Print(L"Core Ring:              53         ");
+    }
+
+    if ( data[offset_maxring] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 8); // h, v ;pos
+	Print(L"Max Ring:               Default    ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 8); // h, v ;pos
+	Print(L"Max Ring:               53         ");
+    }
+
+    if ( data[offset_avx] == 0) {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 9); // h, v ;pos
+	Print(L"Max AVX:                Default    ");
+	} else {
+	uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 43, 9); // h, v ;pos
+	Print(L"Max AVX:                0x1F       ");
+    }
+
+    uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_WHITE|EFI_BACKGROUND_BLACK);
+    uefi_call_wrapper(ST->ConOut->SetCursorPosition, 3, ST->ConOut, 0, 11);
+
+    Print(L" Press B to unlock the bios\n");
+    Print(L" Press V to switch video card\n");
     Print(L" Press H to enable/disable HDMI audio\n");
+    Print(L" Press I to enable/disable Intel XTU\n");
+    Print(L" Press O to enable/disable Overclocking\n");
+    Print(L" Press C to enable/disable CORE speed ratio\n");
+    Print(L" Press R to enable/disable CORE ring ratio\n");
+    Print(L" Press M to enable/disable MAX  ring ratio\n");
+    Print(L" Press A to enable/disable adaptive ratio\n");
+    Print(L" Press X to enable/disable MAX  AVX ratio\n");
 
 
     Print(L" Press ENTER to save new settings\n");
     if ( changes == 0 ) {
 	Print(L" Press any other key or wait to boot without any mods\n");
     } else {
-	Print(L" Press X to boot without any mods                    \n");
+	Print(L" Press E to boot without any mods                    \n");
     }
 
     WaitForSingleEvent(ST->ConIn->WaitForKey, 30000000); // 10000000 = one second
@@ -195,10 +258,70 @@ redraw:
 	    }
 	    efi_input_key = KeyReset;
 	    goto redraw;
-        case 'd':
-	    draw_box(L"Testing 123", 5, 60, 4);
+        case 'i':
+	changes=1;
+	    if ( data[offset_xtu] == 0) {
+		data[offset_xtu] = 0x1;
+	    } else {
+		data[offset_xtu] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
+	    goto redraw;
+        case 'o':
+	changes=1;
+	    if ( data[offset_overclock] == 0) {
+		data[offset_overclock] = 0x1;
+	    } else {
+		data[offset_overclock] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
+	    goto redraw;
+        case 'c':
+	changes=1;
+	    if ( data[offset_ratio] == 0) {
+		data[offset_ratio] = 0x53;
+	    } else {
+		data[offset_ratio] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
+	    goto redraw;
+        case 'r':
+	changes=1;
+	    if ( data[offset_ring] == 0) {
+		data[offset_ring] = 0x53;
+	    } else {
+		data[offset_ring] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
+	    goto redraw;
+        case 'm':
+	changes=1;
+	    if ( data[offset_maxring] == 0) {
+		data[offset_maxring] = 0x53;
+	    } else {
+		data[offset_maxring] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
+	    goto redraw;
+        case 'a':
+	changes=1;
+	    if ( data[offset_adaptive] == 0) {
+		data[offset_adaptive] = 0x1;
+	    } else {
+		data[offset_adaptive] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
 	    goto redraw;
         case 'x':
+	changes=1;
+	    if ( data[offset_avx] == 0) {
+		data[offset_avx] = 0x1f;
+	    } else {
+		data[offset_avx] = 0x0;
+	    }
+	    efi_input_key = KeyReset;
+	    goto redraw;
+        case 'e':
     	    Print(L" Exiting......\n");
     	    WaitForSingleEvent(ST->ConIn->WaitForKey, 10000000); // 10000000 = one second
 	    if ( params == 0){ 
